11535e27bb990dd30fa9bf2221fc486e57cf1cc6
Danicasa <Danicasa@users.noreply.github.com>
2020-12-20 14:24:17 +0000

    Add test to create project

--- microservices-projects-postgresql/src/test/java/com/danicasa/microservices/app/projectControllerTest/ProjectControllerTest.java
+++ microservices-projects-postgresql/src/test/java/com/danicasa/microservices/app/projectControllerTest/ProjectControllerTest.java
@@ -1,5 +1,6 @@
 package com.danicasa.microservices.app.projectControllerTest;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
@@ -11,6 +12,7 @@ import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import com.danicasa.microservicios.app.projects.MicroservicioProjectsApplication;
@@ -53,6 +55,16 @@ public class ProjectControllerTest {
     public void testGetProjectById_fails_when_id_not_exist() {
         Project project = restTemplate.getForObject(getRootUrl() + "/get/1000000", Project.class);
         assertNull(project.getId());
+    }
+    
+    @Test
+    public void testCreateProject() {
+    	Project project = new Project();
+    	project.setName("name for new project");
+    	project.setDescription("description for new project");
+    	ResponseEntity<Project> postResponse =restTemplate.postForEntity(getRootUrl() + "/save", project, Project.class);
+    	assertEquals(postResponse.getStatusCode(), HttpStatus.CREATED);
+    	assertNotNull(postResponse.getBody().getId());
     }
 
 
